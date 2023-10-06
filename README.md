8312ceac4c188e837bc991f2418299d80e056d54
Luis Raziel <razieldiazh@gmail.com>
2023-10-05 21:18:34 -0600

    use useRef to void searching twice the same movie

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ Requerimientos:
 ✅  Necesita mostrar un input para buscar la película y un botón para buscar.
 ✅  Lista las películas encontradas y muestra el título, año y poster.
 ✅ Buscar peliculas por medio del form
-- Hacer fetching de datos
+✅ Hacer fetching de datos
 ✅ Haz que las peliculas se muestren en un grid responsive.
 
 Primera iteración:
-- Evitar que se haga la misma búsqueda dos veces seguidas.
+✅ Evitar que se haga la misma búsqueda dos veces seguidas.
 - Haz que la búsqueda se haga automáticamente al escribir.
 - Evita que se haga la búsqueda continuamente al escribir (debounce).
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { searchMovies } from '../services/movies'
+import { useRef } from 'react'
 export function useMovies({ query }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previusQuery = useRef(query)
 
   const getMovies = async () => {
+    if (query === previusQuery.current) return
     try {
       setLoading(true)
       setError(null)
+      previusQuery.current = query
       const movies = await searchMovies({ query })
       setMovies(movies)
     } catch (error) {
